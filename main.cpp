354d968f52afb0bd152957ce58ed5accad556fc9
Alex <alexppcdsalvc@hotmail.com>
2018-04-13 12:54:48 -0500

    intento de hacer lo de los fps en delta

--- main.cpp
+++ main.cpp
@@ -140,6 +140,13 @@ int main(int argc, char* args[])
 			//Handle events on queue
 			while (SDL_PollEvent(&e) != 0)
 			{
+			    if (timeElapsed>= 1.0f) //mostramos los frames solo cuando haya ocurrido un minuto
+                {
+                    //Devolvemos contador a cero
+                    frameCnt=0;
+                    timeElapsed= 0.0f;
+                }
+
 				if (e.type == SDL_QUIT) ///Salir
 				{
 					GameLoop = true;

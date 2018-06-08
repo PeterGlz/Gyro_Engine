03f2ab853d39a93a8af54461dcdbc5beb5818eb3
TerrierBrakk <felipeortega94@gmail.com>
2018-06-08 11:49:06 -0500

    el del guante es un pendejo...
    por puto que me borro mis cosas pinche maricon....

---
+++ include/Atlas.h
@@ -0,0 +1,16 @@
+#ifndef ATLAS_H
+#define ATLAS_H
+
+
+class Atlas
+{
+    public:
+        Atlas();
+        virtual ~Atlas();
+
+    protected:
+
+    private:
+};
+
+#endif // ATLAS_H
---
+++ src/Atlas.cpp
@@ -0,0 +1,11 @@
+#include "Atlas.h"
+///Pagina que se usara para crear el atlas https://www.leshylabs.com/apps/sstool/
+Atlas::Atlas()
+{
+    //ctor
+}
+
+Atlas::~Atlas()
+{
+    //dtor
+}

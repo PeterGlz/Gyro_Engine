d3d1235248350a48c6de89a6e2bc2b3c76a5e48f
JosZarate <pepe-ds-pokes@hotmail.com>
2018-06-15 12:53:47 -0500

    Casi_completo

--- include/Atlas.h
+++ include/Atlas.h
@@ -1,5 +1,8 @@
 #ifndef ATLAS_H
 #define ATLAS_H
+#include <vector>
+#include <cstring>
+#include <iostream>
 ///Pagina que se usara para crear el atlas https://www.leshylabs.com/apps/sstool/
 
 class Atlas
@@ -16,7 +19,13 @@ class Atlas
         float Width;
         float height;
 
+
     private:
+        std::vector <std::string> Nom;
+        std::vector <float> tX;
+        std::vector <float> tY;
+        std::vector <float> tW;
+        std::vector <float> tH;
 };
 
 #endif // ATLAS_H
--- src/Atlas.cpp
+++ src/Atlas.cpp
@@ -1,10 +1,13 @@
 #include "Atlas.h"
-#include <iostream>
+//#include <iostream>
 #include <fstream>
-#include <string.h>
+//#include <string.h>
 #include <stdio.h>
-#include <cstring>
+//#include <cstring>
 #include <stdlib.h>
+#include <vector>
+#include "RenderGL.h"
+
 
 using namespace std;
 ///Pagina que se usara para crear el atlas https://www.leshylabs.com/apps/sstool/
@@ -20,10 +23,11 @@ Atlas::~Atlas()
 
 void Atlas::Read()
 {
+
     ifstream myfile ("sprites.txt");
 
       char* pch;
-        string line;
+    string line;
 
       if (myfile.is_open())
       {
@@ -36,26 +40,48 @@ void Atlas::Read()
             while (pch != NULL)
             {
                 if(contador == 0)
-                     nombre = pch;
+                {
+                    nombre = pch;
+                    //string st = pch;
+                    Nom.push_back(nombre);
+                }
                 else if(contador == 1)
-                    TopX = atof(pch);
+                {
+                   TopX = atof(pch);
+                    tX.push_back(TopX);
+                }
                 else if(contador == 2)
+                {
                     TopY = atof(pch);
+                    tY.push_back(TopY);
+                }
                 else if(contador == 3)
+                {
                     Width = atof(pch);
+                    ///Width = Width / ->w;
+                    tW.push_back(Width);
+                }
                 else if(contador == 4)
+                {
                     height = atof(pch);
+                    tH.push_back(height);
+                }
                 contador++;
                 pch = strtok (NULL, ",");
             }
-            cout << "Nombre: " << nombre << endl;
-            cout << "TopX: " << TopX << endl;
-            cout << "TopY: " << TopY << endl;
-            cout << "Width: " << Width << endl;
-            cout << "Height: " << height << endl;
+            /*cout << "\nSize : " << g1.size();
+            cout << "\nCapacity : " << g1.capacity();
+            cout << "\nMax_Size : " << g1.max_size();*/
+
+
             delete[] y;
           //cout << line << '\n';
         }
+        cout << "Nombre: " << Nom[0] << endl;
+            cout << "TopX: " << tX[0] << endl;
+            cout << "TopY: " << tY[0] << endl;
+            cout << "Width: " << tW[0] << endl;
+            cout << "Height: " << tH[2] << endl;
         myfile.close();
       }
 
